999e27133d6417b0de328d201d74eb04b9bda1b9
Ali <as598102@icloud.com>
2020-11-26 11:19:12 +0200

    first commit

---
+++ Landmarks Quiz/Landmarks Quiz/Model/Quiz.swift
@@ -0,0 +1,59 @@
+//
+//  Quiz.swift
+//  Landmarks Quiz
+//
+//  Created by Ali  on 21/10/2020.
+//
+
+import Foundation
+import UIKit
+struct Quiz {
+    var score = 0
+    let images = [#imageLiteral(resourceName: "Indonesia 1"),#imageLiteral(resourceName: "Japan 1"),#imageLiteral(resourceName: "China1"),#imageLiteral(resourceName: "India 2"),#imageLiteral(resourceName: "Egypt 2"),#imageLiteral(resourceName: "France 1"),#imageLiteral(resourceName: "USA 2"),#imageLiteral(resourceName: "UAE 1"),#imageLiteral(resourceName: "India 1"),#imageLiteral(resourceName: "UK 1"),#imageLiteral(resourceName: "UAE 3"),#imageLiteral(resourceName: "USA 1"),#imageLiteral(resourceName: "Saudi 1"),#imageLiteral(resourceName: "UAE 2"),#imageLiteral(resourceName: "Egypt 1"),#imageLiteral(resourceName: "Jordan 1")]
+let choices =  [["Indonesia","Japan","China","Korea"],
+  ["Japan","USA","France","Jordan"],
+  ["China","Japan","Indonesia","Korea"],
+  ["India","Pakistan","USA","China"],
+  ["Egypt","Jordan","Syria","Sudan"],
+  ["France","UK","Italy","Brazil"],
+  ["USA","UK","France","Italy"],
+  ["UAE","Saudi Arabia","Kuwait","Bahrain"],
+  ["India","Pakistan","Iran","Turkey"],
+  ["UK","USA","Brazil","Korea"],
+  ["UAE","Bahrain","Kuwait","USA"],
+  ["USA","France","Italy","China"],
+  ["Saudi Arabia","UAE","Bahrain","Oman"],
+  ["UAE","Oman","Bahrain","Jordan"],
+  ["Egypt","Morocco","Tunis","Sudan"],
+  ["Jordan","Egypt","Morocco","Tunis"]]
+    
+    var currentQuestion = 0
+    var gameIsActive = true
+        mutating func checkAnswer(answer:String) -> Bool{
+            if gameIsActive{
+
+        if answer == choices[currentQuestion][0]{
+            print("correct")
+            score+=1
+            return true
+        }else{
+            print("wrong")
+            return false
+        }
+        
+}
+    return false
+    }
+    
+    
+    mutating func nextQuestion() {
+        if currentQuestion >= images.count-1{
+            print("Game is Over")
+            gameIsActive = false
+        }else{
+            currentQuestion+=1
+
+        }
+    }
+    
+}
